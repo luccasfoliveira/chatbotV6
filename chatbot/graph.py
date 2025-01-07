a543c011de9747db9e4d6ffb23ce59819174cb56
Luccas Oliveira <102065560+luccasfoliveira@users.noreply.github.com>
2025-01-07 03:11:45 -0300

    Update graph.py

--- chatbot/graph.py
+++ chatbot/graph.py
@@ -4,21 +4,17 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from chatbot.llm import chatbot_logic
 
-# Inicializa o grafo de estados
 class State(TypedDict):
-    messages: Annotated[list, add_messages]  # Definição do tipo de estado
+    messages: Annotated[list, add_messages]
 
 graph_builder = StateGraph(State)
 
-# Adiciona o nó chatbot ao grafo
 graph_builder.add_node("chatbot", chatbot_logic)
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
 
-# Compila o grafo
 graph = graph_builder.compile()
 
-# Função para processar atualizações no grafo
 def stream_graph_updates(user_input: str):
     responses = []
     for event in graph.stream({"messages": [("user", user_input)]}):
